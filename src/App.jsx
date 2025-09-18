8fe454773be5bff7e8ba5e9196687f5d634f5de7
errun <zhangwei@minimax.ltd>
2025-09-18 18:23:41 +0800

    Fix base path and routing for custom domain deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Layout from './components/Layout'
 
 function App() {
   return (
-    <Router basename="/guruHoldings">
+    <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
